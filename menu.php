15f11d417a651269c2fc3a597689e00d2e203676
JaeHo Song <thruthesky@gmail.com>
2014-04-21 02:23:52 +0800

    working on mall

--- menu.php
+++ menu.php
@@ -1,9 +1,16 @@
-<a href='?module=<?=$module?>&action=config_index'>First Page</a>,
-Item List,
 
-<a href='?module=<?=$module?>&action=config_new_item'>Post New Item</a>,
 
-Payment,
-View Stat,
-Wish Stat,
-Comment List & Stat,
+
+<div class='mall-admin-menu'>
+<a href='?module=<?=$module?>&action=config_index'>First Page</a> | 
+Item List | 
+
+<a href='?module=<?=$module?>&action=config_new_item'>Post New Item</a> | 
+
+Payment | 
+View Stat | 
+Wish Stat | 
+Comment List & Stat
+
+
+</div>
